e54c8a9de88ae5fcd0000d3665faeb31a58f6aae
Anton Vlasov <nanostet@gmail.com>
2021-05-02 18:14:26 +0700

    Fix parameters getting

--- src/utils/getInputs.js
+++ src/utils/getInputs.js
@@ -3,13 +3,24 @@ import * as core from '@actions/core'
 export function getInputs(raw) {
   return Object.keys(raw).reduce((total, key) => {
     const rawType = raw[key]
-    if (rawType === 'boolean' || rawType === 'boolean?') {
-      total[key] = Boolean(core.getInput(key))
-    } else if (rawType === 'number' || rawType === 'number?') {
-      total[key] = Number.parseFloat(core.getInput(key))
-    } else {
-      total[key] = core.getInput(key)
-    }
+
+    onKey(key, (value) => {
+      if (rawType === 'boolean' || rawType === 'boolean?') {
+        total[key] = Boolean(value)
+      } else if (rawType === 'number' || rawType === 'number?') {
+        total[key] = Number.parseFloat(value)
+      } else {
+        total[key] = value
+      }
+    })
+
     return total
   }, {})
+}
+
+function onKey(key, onValue) {
+  const value = core.getInput(key)
+  if (value !== null && value !== undefined) {
+    onValue(value)
+  }
 }
